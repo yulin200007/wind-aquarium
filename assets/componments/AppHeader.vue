47ac71a68c582c0c4f15e3f1986f985d882f2c9f
yulin200007 <yulin200007@gmail.com>
2025-12-25 16:49:19 +0900

    12250449

---
+++ assets/componments/AppHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// 這裡放剛才寫的 TS 邏輯
+const isMenuOpen = ref<boolean>(false)
+const activeGroup = ref<number | null>(null)
+const isLangOpen = ref<boolean>(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+  if (process.client) {
+    document.body.style.overflow = isMenuOpen.value ? 'hidden' : ''
+  }
+}
+
+const toggleGroup = (index: number) => {
+  activeGroup.value = activeGroup.value === index ? null : index
+}
+</script>
+
+<template>
+  <header> ... </header>
+</template>
+
+<style scoped>
+  /* 這裡放原本的 SCSS/CSS */
+</style>
